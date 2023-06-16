92d6feea151149b261d4868bfbbc7e19b5eca8c7
kenneth <knnthgb123@gmail.com>
2023-06-16 22:21:53 +0800

    start

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Enrollment</title>
+</head>
+<body>
+   
+<div id="login">
+    <br>
+        <h1>Log in </h1><br>    
+<form method="post" class="loginform">
+  <div class="mb-3">
+    <label for="InputUname" class="form-label">Username</label>   
+    <input type="text" class="form-control" id="InputUname"  name="username" required>
+    </div>
+    <br>
+  <div class="mb-3">
+    <label for="InputUname" class="form-label">Email</label>   
+    <input type="email" class="form-control" id="InputEmail"  name="email" required>
+    </div>
+    <br>
+  <div class="mb-3">
+    <label for="InputUname" class="form-label">Username</label>   
+    <input type="text" class="form-control" id="InputUname"  name="username" required>
+    </div>
+    </form>
+    <br>
+    <button type="submit" class="submitin" name="inbutton">Log in</button>
+    <br><br>
+    <a href="forgot.php" class="forgot">Forgot password?</a>
+    <p>Don't have an account? <a href="signin" class="signin">Sign in</a></p>
+</div>
+    <!-- <table class="table1">
+        <thead>
+            <tr>
+                <th>
+                    
+                </th>
+            </tr>
+        </thead>
+    </table> -->
+    
+</body>
+</html>
